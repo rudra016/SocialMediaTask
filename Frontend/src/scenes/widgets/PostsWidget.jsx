58f6ee75730f96a2e25f2aa18c74f459e94913ee
Rudra Sharma <94120092+rudra016@users.noreply.github.com>
2024-06-20 22:59:59 +0530

    Update PostsWidget.jsx

--- Frontend/src/scenes/widgets/PostsWidget.jsx
+++ Frontend/src/scenes/widgets/PostsWidget.jsx
@@ -20,7 +20,7 @@ const PostsWidget = ({ userId, loggedUserId, userProfilePicture, isProfile = fal
 
   const getUserPosts = async () => {
     const response = await fetch(
-      'https://taskback-0l0h.onrender.com/posts/${userId}/posts',
+      `https://taskback-0l0h.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
